d8427a1e7d522dc446f971b6f09d451a5dea1ae9
yanling19 <singyanling@gmail.com>
2026-09-04 17:10:09 +0800

    Fun Conditioning

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
+#Activity 1: Create first python program
 print("=================================")
 print("Welcome here")
 print("My first post!")
 print("=================================")
+
+#Activity 2: Update profile & add to repository
+username = "cool_creater"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
