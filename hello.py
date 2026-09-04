e12f472ddd8ede4feaa4b627b4ac79bc5457dd71
yanling19 <singyanling@gmail.com>
2026-09-04 17:11:17 +0800

    Interactive Profile Creater

--- hello.py
+++ hello.py
@@ -24,4 +24,15 @@ print("Day 2:", followers)
 followers += 10
 print("Day 3:", followers)
 
+#Activity 4: Interactive Profile Creator
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
+
 
