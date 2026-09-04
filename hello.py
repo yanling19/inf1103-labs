21a4f60420d888bef47d5024ac71e3fe7fc8bbd4
yanling19 <singyanling@gmail.com>
2026-09-04 17:10:56 +0800

    Follower Growth Tracker

--- hello.py
+++ hello.py
@@ -13,3 +13,15 @@ print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
 
+#Activity 3: Follower Growth Tracker
+followers = 100
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers += 10
+print("Day 3:", followers)
+
+
